5cc1709794b4e87db97cdeb817f9748baed5e1de
CuongVo2003 <cuongvcmfj2003@gmail.com>
2023-12-19 21:29:20 +0700

    init project

---
+++ src/main/java/com/example/tettttt/TetttttApplication.java
@@ -0,0 +1,13 @@
+package com.example.tettttt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetttttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetttttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tettttt/TetttttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tettttt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetttttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
